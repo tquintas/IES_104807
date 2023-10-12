2bcbe11323d02cf6a5bccc4300d4782704c6b11f
Tiago Quintas <tjbquintas@gmail.com>
2023-10-12 11:32:24 +0100

    Create 2.3 project

---
+++ lab2/lab2.3/ServingWeb/src/main/java/ua/tquintas/servingweb/ServingWebApplication.java
@@ -0,0 +1,13 @@
+package ua.tquintas.servingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebApplication.class, args);
+    }
+
+}
---
+++ lab2/lab2.3/ServingWeb/src/test/java/ua/tquintas/servingweb/ServingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.tquintas.servingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
