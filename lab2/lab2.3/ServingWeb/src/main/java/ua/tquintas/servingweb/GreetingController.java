baf527b9a20915d092161a3a520dfd17fd638a33
Tiago Quintas <tjbquintas@gmail.com>
2023-10-12 12:09:47 +0100

    Commented the code to enable the Rest Controller

--- lab2/lab2.3/ServingWeb/src/main/java/ua/tquintas/servingweb/GreetingController.java
+++ lab2/lab2.3/ServingWeb/src/main/java/ua/tquintas/servingweb/GreetingController.java
@@ -1,9 +1,10 @@
-package ua.tquintas.servingweb;
+/*package ua.tquintas.servingweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
@@ -13,3 +14,4 @@ public class GreetingController {
         return "greeting";
     }
 }
+*/
