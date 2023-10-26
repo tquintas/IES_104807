5e1a678e223ec05bb5812f8c0483e3b765d225d5
Tiago Quintas <tjbquintas@gmail.com>
2023-10-26 11:45:00 +0100

    Add Users Entity Class with getters and setters

---
+++ lab3/lab3.1/JPA/src/main/java/ua/tquintas/jpa/Users.java
@@ -0,0 +1,46 @@
+package ua.tquintas.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @Min(value = 0, message = "Age should not be less than 0")
+    @Max(value = 150, message = "Age should not be greater than 150")
+    private int age;
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
