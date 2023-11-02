c9e354a6610293a4be3fd727074745ded86bb849
Tiago Quintas <tjbquintas@gmail.com>
2023-11-02 11:19:53 +0000

    Add a repository class to extend the CrudRepository

---
+++ lab3/lab3.1/JPA/src/main/java/ua/tquintas/jpa/UsersRepository.java
@@ -0,0 +1,7 @@
+package ua.tquintas.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends CrudRepository<Users, Long> {}
