286da9ffde6c33183722c41009d2e9e20dc42c8c
Tiago Quintas <tjbquintas@gmail.com>
2023-11-02 11:20:41 +0000

    Create the application starter

---
+++ lab3/lab3.2/RestMySql/src/main/java/ua/tquintas/restmysql/RestMySqlApplication.java
@@ -0,0 +1,13 @@
+package ua.tquintas.restmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMySqlApplication.class, args);
+    }
+
+}
