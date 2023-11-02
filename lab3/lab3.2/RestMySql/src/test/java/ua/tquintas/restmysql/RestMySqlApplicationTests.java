298c66122e5d834e1b122b5e91ba506219ddf286
Tiago Quintas <tjbquintas@gmail.com>
2023-11-02 11:20:32 +0000

    Create the app

---
+++ lab3/lab3.2/RestMySql/src/test/java/ua/tquintas/restmysql/RestMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.tquintas.restmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
